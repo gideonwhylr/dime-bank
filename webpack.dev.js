18ce0ce9ffa833828517ed91170caca764ca4ef8
lymarrie <lucmarrie1@gmail.com>
2021-05-21 11:20:28 -0400

    added webpack.dev + added hover:underline to all links

---
+++ webpack.dev.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: {
+        index: './src/index.js',
+
+      }, 
+    output: {
+        filename: 'index.bundle.js',
+        path: path.resolve(__dirname, 'desktop'),
+        clean: true
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/template.html",
+      }),
+      new CleanWebpackPlugin()
+  ],
+    module: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: [
+            // Order is last to first
+                'style-loader',
+                {
+                    loader: 'css-loader',
+                    options: {
+                        importLoaders: 1
+                    }
+                }, 
+                'postcss-loader'
+            ],
+          },
+        ],
+      }
+
+};
