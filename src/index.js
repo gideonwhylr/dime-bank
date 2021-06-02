b3838fde950d0586043c38dbb65ea9b2050c38d0
rosiegrant <rosellen.grant@gmail.com>
2021-06-02 15:38:28 -0400

    adding answers via js file instead of in hbs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import './global.css';
 import Icon from './beacon-check.png';
+// import ANSWERS from '@yext/answers-search-ui';
+const ANSWERS = require('@yext/answers-search-ui');
+const TemplateBundle = require('@yext/answers-search-ui/dist/answerstemplates.compiled.min.js');
 
+ANSWERS.init({
+    apiKey: "a381bbbc78e07a078cabe52f321c62eb",  //sample test experience
+    experienceKey: "dime_answers_experience_-_frontend_test",  //sample test experience
+    templateBundle: TemplateBundle.default,
+    businessId: "3179813",  //sample test experience
+    experienceVersion: "PRODUCTION",
+    onReady: function () {
+      // init components
+      this.addComponent("SearchBar", {
+        container: ".search-bar"
+      });
+      this.addComponent("SpellCheck", {
+        container: ".spell-check"
+      });
+      this.addComponent("DirectAnswer", {
+        container: ".direct-answer"
+      });
+      this.addComponent("UniversalResults", {
+        container: ".universal-results"
+      });
+      this.addComponent("LocationBias", {
+        container: ".location-bias"
+      });
+    }
+  });
